c4d2a19853a2fdcf8bb850ddb3ece97dda1f4a0c
barakg <barakgonen9@gmail.com>
2021-02-21 12:45:58 +0200

    Initialized basic consumer and producer

--- src/main/java/org/generic/rest/utils/kafka/consumer/Consumer.java
+++ src/main/java/org/generic/rest/utils/kafka/consumer/Consumer.java
@@ -1,7 +1,52 @@
 package org.generic.rest.utils.kafka.consumer;
 
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.IndexedRecord;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.bg.avro.structures.base.objects.Coordinate;
+
+import java.util.Arrays;
+import java.util.Properties;
+
 public class Consumer {
-    public static void main(String[] args){
+
+    private static void confluentListener(){
+        Properties props = new Properties();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
+
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        props.put("schema.registry.url", "http://localhost:8081");
+
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        String topic = "TEST-TOPIC";
+        final KafkaConsumer<String, Coordinate> consumer = new KafkaConsumer<String, Coordinate>(props);
+        consumer.subscribe(Arrays.asList(topic));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, Coordinate> records = consumer.poll(100);
+                for (ConsumerRecord<String, Coordinate> record : records) {
+                    System.out.printf("offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+    public static void main(String[] args) {
+
+        Coordinate coordinate = Coordinate.newBuilder().setAltitude(1222).setLongitude(23.2).setLatitude(323).build();
+
+        System.out.println(coordinate);
         System.out.println("my consumer");
+        confluentListener();
     }
 }
--- src/main/java/org/generic/rest/utils/kafka/producer/Producer.java
+++ src/main/java/org/generic/rest/utils/kafka/producer/Producer.java
@@ -1,7 +1,38 @@
 package org.generic.rest.utils.kafka.producer;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.SerializationException;
+import org.bg.avro.structures.base.objects.Coordinate;
+
+import java.util.Properties;
+
 public class Producer{
     public static void main(String[] args){
-        System.out.println("my producer");
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        props.put("schema.registry.url", "http://localhost:8081");
+        KafkaProducer producer = new KafkaProducer(props);
+
+        String key = "key1";
+
+        Coordinate coordinate = Coordinate.newBuilder().setAltitude(1222).setLongitude(23.2).setLatitude(323).build();
+        ProducerRecord<String, Coordinate> record = new ProducerRecord<>("TEST-TOPIC", key, coordinate);
+        try {
+            producer.send(record);
+        } catch(SerializationException e) {
+            // may need to do something with it
+        }
+// When you're finished producing records, you can flush the producer to ensure it has all been written to Kafka and
+// then close the producer to free its resources.
+        finally {
+            producer.flush();
+            producer.close();
+        }
     }
 }
